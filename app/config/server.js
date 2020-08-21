1e9cf2878a3ee404d400f57b5b5a636c7e38f06a
Tim Plummer <tplummer@flexion.us>
2020-08-21 10:22:58 -0500

    Spin up docker containers

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1c3ad073ab977d08538adaaedc0f3078'),
+    },
+  },
+});
